e9f9b500eb8205921f83a4be52301c151ea207f1
bhimsur <bhimsur>
2023-02-20 19:37:45 +0700

    :bento: add sample sql file

---
+++ src/main/resources/sql/example-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.article (id, title, body, author, created_date) VALUES (1, 'Tutorial Quarkus', 'Get Started', 'admin', '2023-02-19 17:45:55.182088');
+INSERT INTO public.article (id, title, body, author, created_date) VALUES (2, 'Panache Repository', 'Tutorial', 'guest', '2023-02-19 17:45:55.182088');
